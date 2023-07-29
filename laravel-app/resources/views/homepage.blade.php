7088fc294410b6e15f3ea22f8af4688eb4a3eb47
Vladislav Ilchenko <x7kote7x@gmail.com>
2023-07-29 23:23:49 +0300

    Some changes to homepage
    Signed-off-by: Vladislav Ilchenko <x7kote7x@gmail.com>

--- laravel-app/resources/views/homepage.blade.php
+++ laravel-app/resources/views/homepage.blade.php
@@ -1,12 +1,12 @@
 @extends('layouts.app')
 @section('title')
-    Life Structure
+    Welcome to Life Structure!
 @endsection
 @section('styles')
     <link rel="stylesheet" href="{{asset("/css/homepage.css")}}">
 @endsection
 @section('content')
-    <button class="login btn btn-primary btn-lg" onclick='Livewire.emit("openModal","sign-in-sign-up")'
+    <button class="login" onclick='Livewire.emit("openModal","sign-in-sign-up")'
             type="button">
         Organise my life!
     </button>
