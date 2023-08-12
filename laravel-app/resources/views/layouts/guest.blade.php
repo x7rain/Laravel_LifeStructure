1c4bea3319ca6c3801a884dc66168a1a7f379dcf
Vladislav Ilchenko <x7kote7x@gmail.com>
2023-08-12 15:37:47 +0300

    add page-content wrapper to layout
    Signed-off-by: Vladislav Ilchenko <x7kote7x@gmail.com>

--- laravel-app/resources/views/layouts/guest.blade.php
+++ laravel-app/resources/views/layouts/guest.blade.php
@@ -4,7 +4,7 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <meta name="csrf-token" content="{{ csrf_token() }}">
-    <title>@yield('title', "Life Structure")</title>
+    <title>@yield('title', "LifeController Structure")</title>
     @vite(['resources/css/app.css', 'resources/js/app.js'])
     @livewireStyles
     @yield('styles')
@@ -20,7 +20,9 @@
             </div>
         @endforeach
     </div>
-    @yield('content')
+    <div class="page-content">
+        @yield('content')
+    </div>
 </div>
 @include('layouts.footer')
 @livewireScripts
