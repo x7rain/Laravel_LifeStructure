c80c0f5d392cdd561a3479333ffa0bc85b2e8e34
Vladislav Ilchenko <x7kote7x@gmail.com>
2023-08-12 15:36:55 +0300

    minor visual fixes for navigation
    Signed-off-by: Vladislav Ilchenko <x7kote7x@gmail.com>

--- laravel-app/resources/views/layouts/navigation.blade.php
+++ laravel-app/resources/views/layouts/navigation.blade.php
@@ -1,7 +1,7 @@
 <nav x-data="{ open: false }" class="bg-white border-b border-gray-100">
     <!-- Primary Navigation Menu -->
-    <div class="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
-        <div class="flex justify-between h-16">
+    <div class="mx-auto px-4 sm:px-6 lg:px-8">
+        <div class="flex justify-between">
             <div class="flex">
                 <!-- Navigation Links -->
                 <div class="hidden space-x-8 sm:-my-px sm:ml-10 sm:flex">
@@ -61,7 +61,7 @@
     <div :class="{'block': open, 'hidden': ! open}" class="hidden sm:hidden">
         <div class="pt-2 pb-3 space-y-1">
             <x-responsive-nav-link :href="route('life')" :active="request()->routeIs('life')">
-                {{ __('Life') }}
+                {{ __('LifeController') }}
             </x-responsive-nav-link>
         </div>
 
